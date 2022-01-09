4112732cdddfb27499c70550beb71ecb9bfc8bf4
hesensen <hess@erongdu.com>
2022-01-09 15:11:05 +0800

    测试类调整

--- src/main/java/com/hss/task/MyTask.java
+++ src/main/java/com/hss/task/MyTask.java
@@ -9,10 +9,11 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
-public class myTask {
+public class MyTask {
 
-	 @Scheduled(cron = "0/5 * * * * ? ") // 间隔5秒执行
-	    public void taskCycle() {
-	        System.out.println("使用SpringMVC框架配置定时任务");
-	    }
+	// 间隔5秒执行
+	@Scheduled(cron = "0/5 * * * * ? ")
+	public void taskCycle() {
+		System.out.println("使用SpringMVC框架配置定时任务");
+	}
 }
