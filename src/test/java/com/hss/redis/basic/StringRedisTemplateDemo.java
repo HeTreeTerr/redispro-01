85fdb32772941a0978ef3d40b2e70b1df89fbde4
‘HeTreeTerr’ <3110708879@qq.com>
2022-04-20 22:05:28 +0800

    redis类重新归类

--- src/test/java/com/hss/redis/basic/StringRedisTemplateDemo.java
+++ src/test/java/com/hss/redis/basic/StringRedisTemplateDemo.java
@@ -2,28 +2,32 @@ package com.hss.redis.basic;
 
 import com.hss.service.StringCommandService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hss.service.UserService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * RedisTemplate
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"classpath:spring_redis.xml"})
 public class StringRedisTemplateDemo {
+
+	@Autowired
+	private StringCommandService stringCommandService;
 
 	/**
 	 * 测试string RedisTemplate
 	 */
 	@Test
 	public void t1() {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring_redis.xml");
-
-		StringCommandService stringCommand = ctx.getBean(StringCommandService.class);
 		String key = "applicationName123";
-		String result = stringCommand.getString(key);
+		String result = stringCommandService.getString(key);
 		System.out.println("String返回值："+result);
-		
-		ctx.close();
 	}
 
 }
